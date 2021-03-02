3a9dc18a185e3e7e2bc287694e803c0146ae7291
Eugen Mayer <Dyubomayer@gmail.com>
2021-03-02 13:01:09 +0500

    'junit4'

---
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import service.CashbackHackService;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+        int amount = 2100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void shouldAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldBuyExtraIfTheAmount1000(){
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+
+}
